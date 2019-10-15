37cfdd370a643a6bd555f20a04e7a15a3c1c5eff
Igor200002 <56598401+Igor200002@users.noreply.github.com>
2019-10-15 20:51:41 +0300

    Add files via upload

---
+++ 1лабаКод.m
@@ -0,0 +1,18 @@
+a=2.5
+
+b=1.35
+
+c=-7.2
+
+i=8
+
+x=0.75
+y=(1.5*(a-b)^2)/(abs(a-b)*c)+i/5+10^3*sqrt((abs(c-b)+a^2)/(a+c)^2/3)-((a+x^2)*cos(7))/(i*(x^2)+(a^2)*b*c)
+
+mas = [a b i c x y]
+
+r=max(abs(mas))
+
+mas(6) = y*r
+
+t=min(mas)
